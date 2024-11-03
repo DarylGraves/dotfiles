8e5a7f6a04d4331f0a09a72937eefe5eb8a6d38c
Daryl Graves <d.graves88@outlook.com>
2024-11-03 20:43:10 +0000

    Powershell Profile

--- readonly_OneDrive/3 Resources/OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/3 Resources/OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,9 +2,23 @@ Set-PoshPrompt -Theme slim
 Set-PSReadLineOption -PredictionView List
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
-Import-Module -Name CompletionPredictor
-Import-Module -Name Terminal-Icons
+#################################################
+# Modules
+#################################################
+$RequiredModules = @("CompletionPredictor", "Terminal-Icons", "ImportExcel", "PSWriteHTML", "PwshSpectreConsole")
 
+foreach ($Module in $RequiredModules) {
+	$ModuleInstalled = Get-Module -ListAvailable -SkipEditionCheck -ErrorAction SilentlyContinue -Name $Module
+	if ($null -eq $ModuleInstalled) {
+		Install-Module -Name $Module -Scope CurrentUser -Force
+	}
+
+	Import-Module -Name $Module
+}
+
+#################################################
+# Functions
+#################################################
 function vim {
 	& "C:\Program Files\Vim\vim91\vim.exe" $args && echo "`e[5 q"
 }
