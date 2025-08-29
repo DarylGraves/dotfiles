3b8a679552cd1dcf6c8d634623dadd05e25c8436
Daryl Graves <d.graves88@outlook.com>
2025-08-30 00:22:06 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -86,6 +86,11 @@ foreach ($Module in $RequiredModules) {
 }
 
 #################################################
+# Fzf
+#################################################
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+#################################################
 # Aliases
 #################################################
 Set-Alias -Name "adu" -Value "Get-AdUser"
