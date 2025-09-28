e6992e70e113e2cbfb8f74e64725437ca83d3100
Daryl Graves <d.graves88@outlook.com>
2025-09-28 15:50:54 +0100

    Add Scripts/Linux/i3lock-color/lock.sh

---
+++ Scripts/Linux/i3lock-color/executable_lock.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+i3lock \
+  --color=000000 --indicator \
+  --inside-color=00000000 --ring-color=#FCA000 \
+  --line-color=00000000 --separator-color=#FCA000 \
+  --keyhl-color=#FCA000 --bshl-color=#FCA000 \
+  \
+  --insidever-color=00000000 --ringver-color=#FCA000 --verif-color=#FCA000 --verif-text="" \
+  --insidewrong-color=#330000 --ringwrong-color=#CC8400 --wrong-color=#CC8400 --wrong-text="" \
+  \
+  --layout-color=00000000
+  
