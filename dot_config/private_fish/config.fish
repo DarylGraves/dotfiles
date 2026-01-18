05b0cadc963b938418fdb01daaba980a28f15173
Daryl Graves <d.graves88@outlook.com>
2026-01-18 23:23:45 +0000

    Add .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	fastfetch
+	oh-my-posh init fish --config ~/.config/oh-my-posh/themes/di4am0nd.omp.json | source
+end
+
