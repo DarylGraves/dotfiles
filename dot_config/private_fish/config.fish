4f5eada84ee666892af397561aa0e858427658d5
Daryl Graves <d.graves88@outlook.com>
2026-01-18 23:13:27 +0000

    Cleanup

--- dot_config/private_fish/config.fish
+++
@@ -1,5 +0,0 @@
-fish_add_path ~/.local/bin
-oh-my-posh init fish --config ~/.config/oh-my-posh/slim.omp.json | source
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
