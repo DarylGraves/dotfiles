59cae0f956f7b9c74952c8add1579912338216e3
Daryl Graves <d.graves88@outlook.com>
2026-01-25 17:14:12 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 	fastfetch
 	oh-my-posh init fish --config ~/.config/oh-my-posh/di4am0nd.omp.json | source
-	set -gx EDITOR vim
-	set -gx VISUAL vim
+	set -gx EDITOR nvim
+	set -gx VISUAL nvim
 end
 
