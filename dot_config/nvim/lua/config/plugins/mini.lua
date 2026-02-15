bca897d63c36de8dc308775b97de5f8bca91fa49
Daryl Graves <d.graves88@outlook.com>
2026-02-15 20:38:11 +0000

    Update .config/nvim/lua/config/plugins/mini.lua

--- dot_config/nvim/lua/config/plugins/mini.lua
+++ dot_config/nvim/lua/config/plugins/mini.lua
@@ -2,8 +2,27 @@ return {
   {
      'nvim-mini/mini.nvim',
      config = function()
+       -- Status Line
        local statusline = require 'mini.statusline'
        statusline.setup { use_icons = true }
+       -- Pairs
+       local pairs = require 'mini.pairs'
+       pairs.setup {
+	  modes = { insert = true, command = false, terminal = false },
+	  mappings = {
+	    ['('] = { action = 'open', pair = '()', neigh_pattern = '^[^\\]' },
+	    ['['] = { action = 'open', pair = '[]', neigh_pattern = '^[^\\]' },
+	    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '^[^\\]' },
+
+	    [')'] = { action = 'close', pair = '()', neigh_pattern = '^[^\\]' },
+	    [']'] = { action = 'close', pair = '[]', neigh_pattern = '^[^\\]' },
+	    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '^[^\\]' },
+
+	    ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '^[^\\]',   register = { cr = false } },
+	    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '^[^%a\\]', register = { cr = false } },
+	    ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '^[^\\]',   register = { cr = false } },
+	  },
+       }
      end
   }
 }
