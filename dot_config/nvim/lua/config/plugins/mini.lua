7c858b9f899164235ff4b711ba9ba2c9da8f551c
Daryl Graves <d.graves88@outlook.com>
2026-02-15 20:50:43 +0000

    Update .config/nvim/lua/config/plugins/mini.lua

--- dot_config/nvim/lua/config/plugins/mini.lua
+++ dot_config/nvim/lua/config/plugins/mini.lua
@@ -5,6 +5,18 @@ return {
        -- Status Line
        local statusline = require 'mini.statusline'
        statusline.setup { use_icons = true }
+
+       local comment = require 'mini.comment'
+       comment.setup {
+	 mappings = {
+	    -- Toggle comments with Ctrl+/
+	    comment = '<C-_>',
+	    comment_line = '<C-_>',
+	    comment_visual = '<C-_>',
+	    textobject = '<C-_>',
+	  },
+       }
+
        -- Pairs
        local pairs = require 'mini.pairs'
        pairs.setup {
