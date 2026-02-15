5c04353210e266b3ec70e3403254aba7273a7e28
Daryl Graves <d.graves88@outlook.com>
2026-02-15 23:55:41 +0000

    Update .config/nvim/lua/config/plugins/lsp.lua Add .config/nvim/lua/config/plugins/roslyn.lua

--- dot_config/nvim/lua/config/plugins/lsp.lua
+++ dot_config/nvim/lua/config/plugins/lsp.lua
@@ -1,10 +1,41 @@
 return {
   {
     'mason-org/mason-lspconfig.nvim',
-    opts = {},
     dependencies = {
-      { 'mason-org/mason.nvim', opts = {} },
+      {
+        'mason-org/mason.nvim',
+        opts = {
+          registries = {
+            'github:mason-org/mason-registry',
+            'github:Crashdummyy/mason-registry',
+          },
+        },
+      },
       'neovim/nvim-lspconfig',
     },
+    config = function()
+      local lspconfig = require('lspconfig')
+
+      -- We put everything inside the main setup call.
+      -- This bypasses the setup_handlers nil error entirely.
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls', 'pyright', 'powershell_es' },
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({})
+          end,
+        },
+      })
+
+      -- Diagnostics UI
+      vim.diagnostic.config({
+        virtual_text = {
+          spacing = 4,
+          prefix = '●',
+        },
+        severity_sort = true,
+        float = { border = 'rounded' },
+      })
+    end,
   },
 }
