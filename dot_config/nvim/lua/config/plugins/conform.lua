a7b3790b1b63dbfc027cce1ea76de4b2ab3b813d
Daryl Graves <d.graves88@outlook.com>
2026-02-15 21:38:23 +0000

    Add .config/nvim/lua/config/plugins/blink.lua Add .config/nvim/lua/config/plugins/conform.lua Add .config/nvim/lua/config/plugins/guess-indent.lua Add .config/nvim/lua/config/plugins/mason.lua

---
+++ dot_config/nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+    config = function(_, opts)
+      require("conform").setup(opts)
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+          require("conform").format({ bufnr = args.buf })
+        end,
+      })
+    end,
+  }
+}
