7bcb4f2377e5f91d63b15034f8da3c7345840e3a
Daryl Graves <d.graves88@outlook.com>
2026-02-16 00:22:27 +0000

    Add .config/nvim/lua/config/plugins/telescope.lua

---
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local builtin = require('telescope.builtin')
+
+      -- 1. Ctrl+P: Find Files (local project files)
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+
+      -- 2. Ctrl+S: Find Files in Neovim config
+      vim.keymap.set('n', '<C-s>', function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath('config'), -- Automatically finds your ~/.config/nvim
+          hidden = true, -- Shows init.lua and other dotfiles
+        })
+      end, { desc = 'Telescope find nvim config' })
+
+      -- Basic Telescope setup
+      require('telescope').setup({
+        extensions = {
+          ['fzf'] = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
+        },
+      })
+
+      -- Load the fzf extension if it's installed
+      pcall(require('telescope').load_extension, 'fzf')
+    end,
+  },
+}
