e5f524ba2d355fad74e109a017b251196bc2099c
Daryl Graves <d.graves88@outlook.com>
2026-04-12 21:43:17 +0100

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Add .config/nvim/lua/config/plugins/lualine.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -28,7 +28,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 
--- Tab between Buffers
+-- Tab between buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>')
 vim.keymap.set('n', '<S-Tab>', ':bprev<CR>')
 
