2bc509745ab3576c551c65ab5b44b2f8fba6e3dc
Daryl Graves <d.graves88@outlook.com>
2026-04-26 10:56:11 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,8 @@
+fish_add_path ~/Applications
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	fastfetch
-	fish_add_path ~/Applications
 	oh-my-posh init fish --config ~/.config/oh-my-posh/di4am0nd.omp.json | source
 	set -gx EDITOR nvim
 	set -gx VISUAL nvim
