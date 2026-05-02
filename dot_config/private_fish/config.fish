16021fe7762319a4969542e4c80380c211103d03
Daryl Graves <d.graves88@outlook.com>
2026-05-02 22:04:41 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,5 +4,6 @@ if status is-interactive
 	oh-my-posh init fish --config ~/.config/oh-my-posh/di4am0nd.omp.json | source
 	set -gx EDITOR nvim
 	set -gx VISUAL nvim
+	set -gx KUBECONFIG ~/.kube/config
 end
 
