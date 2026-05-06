f5cea325da342694fadaca50724084469e415f9a
Daryl Graves <d.graves88@outlook.com>
2026-05-06 23:26:16 +0100

    Update .config/nvim/lua/config/plugins/neovim-session-manager.lua

--- dot_config/nvim/lua/config/plugins/neovim-session-manager.lua
+++ dot_config/nvim/lua/config/plugins/neovim-session-manager.lua
@@ -1,11 +1,22 @@
 return {
   'Shatur/neovim-session-manager',
-  opts = {},
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,
   config = function()
     local config = require('session_manager.config')
     require('session_manager').setup({
-      autoload_mode = config.AutoloadMode.CurrentDir,
+      -- This order tells it: "Check the current folder. If nothing exists, STOP."
+      autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.Disabled },
+      
       autosave_last_session = true,
+      
+      -- This is critical when running just 'nvim' without arguments. 
+      -- It forces the plugin to ignore the "Global Last Session".
+      autoload_ignore_not_last_session = true,
+      
+      -- Prevents the plugin from trying to "be helpful" and find a 
+      -- session in a parent directory (like your home folder).
+      autosave_only_in_session = true, 
     })
-    end,
+  end,
 }
