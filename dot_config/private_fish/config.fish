f30f03c7985b9a1b5c88ffba97bd766e8e60cf35
Daryl Graves <d.graves88@outlook.com>
2026-06-19 21:27:52 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 	fastfetch
 	oh-my-posh init fish --config ~/.config/oh-my-posh/di4am0nd.omp.json | source
 	alias pomo='bash ~/.config/fish/scripts/pomodoro.sh'
+	alias ls='ls -alh'
 	set -gx EDITOR nvim
 	set -gx VISUAL nvim
 	set -gx KUBECONFIG ~/.kube/config
